d66cc14efc36f5a17ee0d977a0353537251a0008
lofi boy <lofiboyog@gmail.com>
2025-11-16 07:53:12 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/blog',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
