af64768b18ce955229d3e75cc40f3631b2ec81db
lofi boy <lofiboyog@gmail.com>
2025-11-16 08:51:05 +0000

    for app logo use logo.png file

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Image src="/favicon.ico" alt="Caffeine Compass Logo" width={32} height={32} />
+          <Image src="/logo.png" alt="Caffeine Compass Logo" width={32} height={32} />
           <span className="font-bold font-headline text-lg sm:inline-block">
             Caffeine Compass
           </span>
